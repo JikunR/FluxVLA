055006cdb8bc20bdbb0d4ac990a70805f4630659
jace <jace@limxdynamics.com>
2026-04-10 15:06:47 +0800

    feat. gr00t hud04 full finetune config from aloha

---
+++ configs/gr00t/gr00t_hud04_full_finetune.py
@@ -0,0 +1,173 @@
+# Copyright 2026 Limx Dynamics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+model = dict(
+    type='LlavaVLA',
+    pretrained_name_or_path=  # noqa: E251
+    './checkpoints/GR00T-N1.5-3B',
+    vlm_backbone=dict(
+        type='EagleBackbone',
+        vlm_path=  # noqa: E251
+        'fluxvla/models/third_party_models/eagle2_hg_model',
+        vlm_config=dict(max_input_seq_len=900)),
+    vla_head=dict(
+        type='FlowMatchingHead',
+        state_dim=64,
+        hidden_size=1024,
+        input_embedding_dim=1536,
+        num_layers=1,
+        num_heads=4,
+        num_inference_timesteps=4,
+        num_steps=32,
+        traj_length=10, # no use param
+        action_dim=64, # from 32 expand to 64
+        ori_action_dim=41),
+    freeze_vlm_backbone=False,
+    name_mapping={
+        'vlm_backbone.vlm': 'backbone.eagle_model',
+        'vla_head': 'action_head'
+    },
+    freeze_projector=False)
+
+inference_model = dict(
+    type='LlavaVLA',
+    pretrained_name_or_path=  # noqa: E251
+    './checkpoints/GR00T-N1.5-3B',
+    vlm_backbone=dict(
+        type='EagleInferenceBackbone',
+        vlm_path=  # noqa: E251
+        'fluxvla/models/third_party_models/eagle2_hg_model',
+        vlm_config=dict(max_input_seq_len=900)),
+    vla_head=dict(
+        type='FlowMatchingInferenceHead',
+        state_dim=64,
+        hidden_size=1024,
+        input_embedding_dim=1536,
+        num_layers=1,
+        num_heads=4,
+        num_steps=32,
+        num_inference_timesteps=4,
+        traj_length=10,
+        ori_action_dim=41,
+        action_dim=64,
+        max_input_seq_len=900,
+        diffusion_model_cfg=dict(
+            attention_head_dim=48,
+            cross_attention_dim=2048,
+            dropout=0.2,
+            final_dropout=True,
+            interleave_self_attention=True,
+            norm_type='ada_norm',
+            num_attention_heads=32,
+            num_layers=16,
+            output_dim=1024,
+            positional_embeddings=None)))
+
+train_dataloader = dict(
+    per_device_batch_size=8,
+    per_device_num_workers=4,
+    dataset=dict(
+        type='DistributedRepeatingDataset',
+        name_mappings={
+            'observation.state': ['proprio'],
+            'action': ['action']
+        },
+        statistic_keys=['observation.state', 'timestamp', 'action'],
+        # statistic_name='hud04_water',
+        datasets=[
+            dict(
+                type='ParquetDataset',
+                data_root_path=  # noqa: E251
+                [
+                    '/home/jace/dataset/real/loco-mani/wbt_water/loco_mani_wbt_water_0404',  # noqa: E501
+                ],
+                transforms=[
+                    dict(
+                        type='ProcessParquetInputs',
+                        embodiment_id=0,
+                        parquet_keys=[
+                            'observation.state',
+                            'timestamp', 'actions', 'info', 'stats',
+                            'action_masks'
+                        ],
+                        video_keys=[
+                            'observation.images.head',
+                        ],
+                        name_mappings={'observation.state': ['states']}),
+                    dict(type='ParquetPrompter'),
+                    dict(
+                        type='ProcessPromptsWithImage',
+                        max_len=900,
+                        num_images=1,
+                        tokenizer=dict(
+                            type='PretrainedTokenizer',
+                            model_path=  # noqa: E251
+                            'fluxvla/models/third_party_models/eagle2_hg_model',  # noqa: E501
+                            # special_tokens={'pad_token': '<PAD>'}
+                        )),
+                    dict(type='ResizeImages', height=224, width=224),
+                    dict(
+                        type='NormalizeImages',
+                        means=[[123.515625, 116.04492188, 103.59375],
+                               [123.515625, 116.04492188, 103.59375],
+                               [123.515625, 116.04492188, 103.59375]],
+                        stds=[[58.27148438, 57.02636719, 57.27539062],
+                              [58.27148438, 57.02636719, 57.27539062],
+                              [58.27148438, 57.02636719, 57.27539062]],
+                    ),
+                    dict(
+                        type='NormalizeStatesAndActions',
+                        state_dim=64,
+                        action_dim=64,
+                        state_key='proprio',
+                        action_key='action',
+                        norm_type='min_max')
+                ],
+                action_window_size=32)
+        ]))
+
+runner = dict(
+    type='FSDPTrainRunner',
+    max_epochs=6,
+    learning_rate=2e-5,
+    weight_decay=0.0,
+    max_grad_norm=1.0,
+    sampler=None,
+    tokenizer=dict(
+        type='PretrainedTokenizer',
+        model_path=  # noqa: E251
+        'fluxvla/models/third_party_models/eagle2_hg_model',
+        # special_tokens={'pad_token': '<PAD>'}
+    ),
+    collator=dict(
+        type='DictCollator',
+        keys=[
+            'states', 'timestamp', 'images', 'img_masks',
+            'lang_tokens', 'lang_masks', 'actions', 'action_masks',
+            'embodiment_ids'
+        ],
+        meta_keys=['task_description', 'prompt', 'info', 'stats']),
+    metric=dict(
+        type='VLAMetric',
+        active_trackers=('jsonl', 'wandb'),
+        run_dir='work_dirs',
+        grad_accumulation_steps=1,
+        window_size=1),
+    lr_scheduler_type='constant',
+    warmup_ratio=0.0,
+    enable_gradient_checkpointing=False,
+    enable_mixed_precision_training=True,
+    mixed_precision_dtype='bf16',
+    change_key_name=False)
+
